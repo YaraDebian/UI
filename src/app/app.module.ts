95687f70c8fed9faee1c1aab1a75476248a08f2d
Yara Debian <yara.debian@hedgeguard.com>
2021-10-26 23:00:33 +0300

    creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ShowContactComponent } from './show-contact/show-contact.component';
+import { AddEditContactComponent } from './add-edit-contact/add-edit-contact.component';
+import { ContactService } from './contact.service';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LogContactComponent } from './log-contact/log-contact.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { DxDataGridModule, DxButtonModule } from 'devextreme-angular';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ShowContactComponent,
+    AddEditContactComponent,
+    LogContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule,
+    Ng2OrderModule,
+    NgxPaginationModule,
+    DxDataGridModule,
+    DxButtonModule
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
